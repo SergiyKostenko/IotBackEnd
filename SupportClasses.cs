99cb58fc072c92023c9b619381e1fe5ba5cc571b
Sergiy Kostenko <skostenko.extpar@Commonwealth.com>
2021-10-18 21:47:22 +0300

    fixed humidity type

--- SupportClasses.cs
+++ SupportClasses.cs
@@ -9,7 +9,7 @@ namespace IotBackEnd
 
     public class MyTableEntity : TableEntity
     {
-        public Int64 humidity { get; set; }
+        public double humidity { get; set; }
         public double temperature { get; set; }
         public bool isFlameDetected { get; set; }
         public double pressure { get; set; }
